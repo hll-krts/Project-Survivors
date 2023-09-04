7f6eb53e0a8dc0d3fc6a2d2f1eafb7cbd4ed881f
DoctorAquisio <khalil.karatas@gmail.com>
2023-09-04 18:33:12 +0300

    04/09/2023 last updates

---
+++ 04.09.2023/Assets/UsedAssets/MainGirl/sc_MainWeapon.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_MainWeapon : MonoBehaviour
+{
+    [SerializeField] int WeaponLevel = 0, startingLevel = 0;
+    [SerializeField] float baseProjectileCount = 1, baseFireRate = 2f;
+    public float baseDamage = 10f, baseSpeed = 10f, baseLifeTime = 3f, projectileCount, pireRate;
+
+    public sc_normalAtk normalAttackScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartBallzRoutine();
+        projectileCount = baseProjectileCount;
+        pireRate = baseFireRate;
+        WeaponLevel = startingLevel;
+
+        LevelWeaponUP();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) LevelWeaponUP();
+    }
+
+    public void LevelWeaponUP()
+    {
+        if(WeaponLevel < 6) WeaponLevel++;
+
+        switch (WeaponLevel)
+        {
+            case 1:
+                normalAttackScript.damage = baseDamage;
+                normalAttackScript.speed = baseSpeed;
+                normalAttackScript.lifeTime = baseLifeTime;
+                break;
+            case 2:
+                normalAttackScript.damage += normalAttackScript.damage * 20 / 100;
+                break;
+            case 3:
+                normalAttackScript.speed += normalAttackScript.speed * 20 / 100;
+                projectileCount++;
+                break;
+            case 4:
+                normalAttackScript.damage += normalAttackScript.damage * 20 / 100;
+                break;
+            case 5:
+                normalAttackScript.speed += normalAttackScript.speed * 20 / 100;
+                break;
+            case 6:
+                normalAttackScript.damage += normalAttackScript.damage * 2;
+                break;
+        }
+    }
+
+    public void StartBallzRoutine() => StartCoroutine(Ballz());
+    public void StopBallzRoutine() => StopCoroutine(Ballz());
+    IEnumerator Ballz()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(pireRate);
+            
+            for (int i = 0; i < projectileCount; i++)
+            {
+                SummonBallz();
+            }
+            
+        }
+    }
+    void SummonBallz()
+    {
+        Instantiate(normalAttackScript.gameObject, this.transform);
+    }
+}
---
+++ 04.09.2023/Assets/sc_Spawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_Spawner : MonoBehaviour
+{
+    public GameObject StandardWarriorPrefab, kamera;
+    public GameObject[] SpawnLocations;
+
+    bool alive;
+    float SpawnRate = 3f;
+
+    sc_player playerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = FindObjectOfType<sc_player>();
+        alive = playerScript.alive;
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alive = playerScript.alive;
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while(alive)
+        {
+            yield return new WaitForSeconds(SpawnRate);
+            Transform SpawnLoc = SpawnLocations[Random.Range(0, 2)].transform;
+            SpawnLoc.position = SpawnLoc.position + new Vector3(0, Random.Range(-10,11), 0);
+            Instantiate(StandardWarriorPrefab, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+        }
+    }
+
+}
